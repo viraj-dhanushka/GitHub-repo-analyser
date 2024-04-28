fd84afbb61a456dde30db78c81aa516cb22ea04a
VIraj <e16083@eng.pdn.ac.lk>
2024-04-28 11:30:56 +0530

    Comment path replace

--- webapp_frontend/src/routes.js
+++ webapp_frontend/src/routes.js
@@ -40,6 +40,6 @@ export default function Router() {
         { path: '*', element: <Navigate to="/404" /> }
       ]
     },
-    { path: '*', element: <Navigate to="/404" replace /> }
+    // { path: '*', element: <Navigate to="/404" replace /> }
   ]);
 }
