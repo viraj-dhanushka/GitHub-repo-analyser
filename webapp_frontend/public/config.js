b266d2dfcff622174864709498f76751b07c32c0
VIraj <e16083@eng.pdn.ac.lk>
2024-04-28 08:59:15 +0530

    Change configurations

--- webapp_frontend/public/config.js
+++ webapp_frontend/public/config.js
@@ -1,11 +1,11 @@
 window.config = {
   REACT_APP_DEFAULT_TAG:"Not Specified",
-  REACT_APP_BASE_URL: "http://localhost:9090/analyse",
+  REACT_APP_BASE_URL: "/choreo-apis/web-portal/bff-service/github-analyser-backend-c09/v1.0",
     REACT_APP_ORG_NAME: "ESLE-Org",
     REACT_APP_ORG_DISP_NAME: "ESLE",
     auth: {
-      signInRedirectURL: "http://localhost:3000",
-      signOutRedirectURL: "http://localhost:3000",
+      signInRedirectURL: "https://95976c15-5127-4fdc-844b-728537425044.e1-us-east-azure.choreoapps.dev",
+      signOutRedirectURL: "https://95976c15-5127-4fdc-844b-728537425044.e1-us-east-azure.choreoapps.dev",
       clientID: "QScqUpPZAz5QFqRnHW6B1HHevM0a",
       baseUrl: "https://api.asgardeo.io/t/esle",
       scope: [ "openid","profile" ]
