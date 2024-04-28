e02911102ee20bff7ebcf9627532e3e3b1f73f7c
VIraj <e16083@eng.pdn.ac.lk>
2024-04-28 11:22:46 +0530

    Update configs

--- webapp_frontend/public/config.js
+++ webapp_frontend/public/config.js
@@ -4,8 +4,8 @@ window.config = {
     REACT_APP_ORG_NAME: "ESLE-Org",
     REACT_APP_ORG_DISP_NAME: "ESLE",
     auth: {
-      signInRedirectURL: "https://95976c15-5127-4fdc-844b-728537425044.e1-us-east-azure.choreoapps.dev",
-      signOutRedirectURL: "https://95976c15-5127-4fdc-844b-728537425044.e1-us-east-azure.choreoapps.dev",
+      signInRedirectURL: "https://95976c15-5127-4fdc-844b-728537425044.e1-us-east-azure.choreoapps.dev/auth/login/callback",
+      signOutRedirectURL: "https://95976c15-5127-4fdc-844b-728537425044.e1-us-east-azure.choreoapps.dev/auth/logout/callback",
       clientID: "QScqUpPZAz5QFqRnHW6B1HHevM0a",
       baseUrl: "https://api.asgardeo.io/t/esle",
       scope: [ "openid","profile" ]
