6cd63b09735e29ba8f8b056fd959f85b81b4d2c8
VIraj <e16083@eng.pdn.ac.lk>
2024-04-28 12:22:07 +0530

    Get token from asgardeo

--- webapp_frontend/src/utils/axiosInstance.js
+++ webapp_frontend/src/utils/axiosInstance.js
@@ -3,33 +3,29 @@
 
 import { useAuthContext } from '@asgardeo/auth-react';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { getToken } from './oauth';
+// import { useEffect, useState } from 'react';
+// import { getToken } from './oauth';
 
 const API_BASE_URL = window.config.REACT_APP_BASE_URL;
 
 export default function axiosClient() {
 
-  // const { getAccessToken } = useAuthContext();
+  const { getAccessToken } = useAuthContext();
+  return createAxiosInstance(getAccessToken);
 
-  // const [setToken, token] = useState('');
+}
 
-//   useEffect(() => {
-//     getAccessToken().then((accessToken) => {
-//         console.log(accessToken);
-//         // setToken(accessToken)
-//     }).catch((error) => {
-//         console.log(error);
-//     });
-// }, []);
+const createAxiosInstance = async (getAccessToken) => {
+  const token = await getAccessToken();
 
   const axiosInstance = axios.create({
     // baseURL: "http://localhost:9090/analyse",
     baseURL: API_BASE_URL,
-    // TODO: uncomment this and confihure
-    // headers: {
-    //   Authorization: `API-Key ${token}`
-    // }
+    // TODO: uncomment this and configure
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
   });
   return axiosInstance;
-}
+};
